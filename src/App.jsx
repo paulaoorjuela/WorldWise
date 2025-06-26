828c89094d8debab69aee89b285860a089aab01b
Paula <paulaoorjuela@gmail.com>
2025-06-26 18:21:01 -0500

    add CSS modules and missing components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
 import HomePage from './pages/HomePage'
 import Error404 from './pages/Error404'
+import AppLayout from './pages/AppLayout'
+import Login from './pages/Login'
 
 function App() {
 
@@ -13,6 +14,8 @@ function App() {
         <Route path='/' element={<HomePage />}></Route>
         <Route path='product' element={<Product />}></Route>
         <Route path='pricing' element={<Pricing />}></Route>
+        <Route path='login' element={<Login />}></Route>
+        <Route path='app' element={<AppLayout />}></Route>
         <Route path='*' element={<Error404 />}></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import AppNav from "../components/AppNav"
+
+function AppLayout() {
+    return (
+        <div>
+            <AppNav />
+            <h1>App layout</h1>
+        </div>
+    )
+}
+
+
+export default AppLayout
