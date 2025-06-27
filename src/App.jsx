d403bbd55b3a8e6f7575917e9b8c49257e23673e
Paula <paulaoorjuela@gmail.com>
2025-06-26 19:28:12 -0500

    add json-server and fetchCities function

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,51 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Product from './pages/Product'
-import Pricing from './pages/Pricing'
-import HomePage from './pages/HomePage'
-import Error404 from './pages/Error404'
-import AppLayout from './pages/AppLayout'
-import Login from './pages/Login'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import Error404 from "./pages/Error404";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
+const BASE_URL = "http://localhost:8000";
 function App() {
+  const [cities, setCities] = useState({});
+  const [loading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading the data ...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities()
+  }, []);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<HomePage />}></Route>
-        <Route path='product' element={<Product />}></Route>
-        <Route path='pricing' element={<Pricing />}></Route>
-        <Route path='login' element={<Login />}></Route>
-        <Route path='app' element={<AppLayout />}>
-          <Route index element={<p>list of cities</p>}></Route>
-          <Route path='cities' element={<p>list of cities</p>}></Route>
-          <Route path='countries' element={<p>list of countries</p>}></Route>
-          <Route path='form' element={<p>Form</p>}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList />}></Route>
+          <Route path="cities" element={<CityList />}></Route>
+          <Route path="countries" element={<p>list of countries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
         </Route>
-        <Route path='*' element={<Error404 />}></Route>
+        <Route path="*" element={<Error404 />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CityList.jsx
@@ -0,0 +1,9 @@
+import styles from "./CityList.module.css"
+
+function CityList() {
+    return (
+        <div className={styles.cityList}>CityList</div>
+    )
+}
+
+export default CityList
