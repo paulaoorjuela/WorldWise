c052ef69eba5173ec3b928fc404f77fdfda1df56
Paula <paulaoorjuela@gmail.com>
2025-07-07 17:16:25 -0500

    add CitiesContext and custom hook useCities

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
@@ -6,49 +6,31 @@ import Error404 from "./pages/Error404";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
-const BASE_URL = "http://localhost:8000";
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json()
-        setCities(data);
-      } catch {
-        alert("There was an error loading the data ...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities()
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />}></Route>
-        <Route path="product" element={<Product />}></Route>
-        <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} loading={isLoading} />}></Route>
-          <Route path="cities" element={<CityList cities={cities} loading={isLoading}/>}></Route>
-          <Route path="cities/:id" element={<City />}></Route>
-          <Route path="countries" element={<CountryList cities={cities} loading={isLoading}/>}></Route>
-          <Route path="form" element={<Form />}></Route>
-        </Route>
-        <Route path="*" element={<Error404 />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />}></Route>
+            <Route path="cities" element={<CityList />}></Route>
+            <Route path="cities/:id" element={<City />}></Route>
+            <Route path="countries" element={<CountryList />}></Route>
+            <Route path="form" element={<Form />}></Route>
+          </Route>
+          <Route path="*" element={<Error404 />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,9 +1,12 @@
+import { useCities } from "../contexts/CitiesContext";
 import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+    const {cities, isLoading} = useCities()
+
     if (isLoading) return <Spinner />;
     if (!cities.length)
         return <Message message={"Add a city by clicking on a city on the map"} />;
