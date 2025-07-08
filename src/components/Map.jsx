8adb603c5de04b8e4aa251f7805f67987b6a5bdb
Paula <paulaoorjuela@gmail.com>
2025-07-08 18:43:44 -0500

    add map interactions

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,25 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-    const navigate = useNavigate();
     const { cities } = useCities();
     const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
+    const [searchParams] = useSearchParams();
+    const mapLat = searchParams.get("lat");
+    const mapLng = searchParams.get("lng");
+
+    useEffect(function(){
+        if(mapLat && mapLng) setMapPosition([mapLat, mapLng])
+    }, [mapLat, mapLng])
     return (
         <div className={styles.mapContainer}>
         <MapContainer
             center={mapPosition}
-            zoom={13}
+            zoom={6}
             scrollWheelZoom={true}
             className={styles.map}
         >
@@ -31,12 +34,25 @@ function Map() {
                 </Popup>
             </Marker>
             ))}
+            <ChangeCenter position={mapPosition} />
+            <DetectClick />
         </MapContainer>
-        <button onClick={() => setSearchParams({ lat: 23, lng: 12 })}>
-            change position
-        </button>
         </div>
     );
+}
+
+function ChangeCenter({position}){
+    const map = useMap()
+    map.setView(position)
+    return null
+}
+
+function DetectClick(){
+    const navigate = useNavigate();
+
+    useMapEvents({
+        click: e => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+    })
 }
 
 export default Map;
