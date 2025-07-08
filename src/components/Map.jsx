b977c6d8e642b6887e12332032adfc2622341584
Paula <paulaoorjuela@gmail.com>
2025-07-08 13:56:47 -0500

    add leaflet map, and update Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,37 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
     const navigate = useNavigate();
+    const { cities } = useCities();
+    const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
+
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
     return (
-        <div className={styles.mapContainer} onClick={() => navigate("form")}>
-        <h1>Map</h1>
-        <h1>
-            Position {lat} and {lng}
-        </h1>
+        <div className={styles.mapContainer}>
+        <MapContainer
+            center={mapPosition}
+            zoom={13}
+            scrollWheelZoom={true}
+            className={styles.map}
+        >
+            <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+            />
+            {cities.map((city) => (
+            <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+                <Popup>
+                <span>{city.emoji}</span> <span>{city.cityName}</span>
+                </Popup>
+            </Marker>
+            ))}
+        </MapContainer>
         <button onClick={() => setSearchParams({ lat: 23, lng: 12 })}>
             change position
         </button>
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -47,4 +47,5 @@ function useCities(){
     return context
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export {CitiesProvider, useCities };
