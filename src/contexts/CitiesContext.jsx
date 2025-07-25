288f71d1292d25349b12f6765b03ec5440bdc7be
Paula <paulaoorjuela@gmail.com>
2025-07-24 19:25:05 -0500

    update: CitiesContext, add useCallbak, to prevent infinite renders context based

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,5 +1,6 @@
 import {
     createContext,
+    useCallback,
     useContext,
     useEffect,
     useReducer,
@@ -70,7 +71,7 @@ function CitiesProvider({ children }) {
         fetchCities();
     }, []);
 
-    async function getCity(id) {
+    const getCity = useCallback(async function getCity(id) {
         if(Number(id) === currentCity.id) return
         dispatch({ type: "loading" });
         try {
@@ -83,7 +84,7 @@ function CitiesProvider({ children }) {
             payload: "There was an error loading the city ...",
         });
         }
-    }
+    }, [currentCity.id])
 
     async function createCity(newCity) {
         dispatch({ type: "loading" });
